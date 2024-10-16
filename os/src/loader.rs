eb3238a9099e61d108a280d20041ac86c1cfa956
lurenjia1213 <30799041+lurenjia1213@users.noreply.github.com>
2024-10-16 18:01:36 +0800

    commo

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -58,14 +58,14 @@ pub fn get_num_app() -> usize {
     extern "C" {
         fn _num_app();
     }
-    unsafe { (_num_app as usize as *const usize).read_volatile() }
+    unsafe { (_num_app as usize as *const usize).read_volatile() }//返回app地址
 }
 
 /// Load nth user app at
 /// [APP_BASE_ADDRESS + n * APP_SIZE_LIMIT, APP_BASE_ADDRESS + (n+1) * APP_SIZE_LIMIT).
 pub fn load_apps() {
     extern "C" {
-        fn _num_app();
+        fn _num_app();//os/src/link_app.S _num_app
     }
     let num_app_ptr = _num_app as usize as *const usize;
     let num_app = get_num_app();
@@ -76,21 +76,21 @@ pub fn load_apps() {
     }
     // load apps
     for i in 0..num_app {
-        let base_i = get_base_i(i);
+        let base_i = get_base_i(i);//这里会决定内存区域
         // clear region
-        (base_i..base_i + APP_SIZE_LIMIT)
+        (base_i..base_i + APP_SIZE_LIMIT)//闭包，清零base_i到base_i+APP_SIZE_LIMIT这段区域的内存
             .for_each(|addr| unsafe { (addr as *mut u8).write_volatile(0) });
         // load app from data section to memory
         let src = unsafe {
             core::slice::from_raw_parts(app_start[i] as *const u8, app_start[i + 1] - app_start[i])
         };
         let dst = unsafe { core::slice::from_raw_parts_mut(base_i as *mut u8, src.len()) };
-        dst.copy_from_slice(src);
+        dst.copy_from_slice(src);//拷贝到执行区域
     }
 }
 
 /// get app info with entry and sp and save `TrapContext` in kernel stack
-pub fn init_app_cx(app_id: usize) -> usize {
+pub fn init_app_cx(app_id: usize) -> usize {//初始化上下文
     KERNEL_STACK[app_id].push_context(TrapContext::app_init_context(
         get_base_i(app_id),
         USER_STACK[app_id].get_sp(),
