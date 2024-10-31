375b52c3aa1510b0dbedcc76e81b881e4cd35d44
lurenjia1213 <30799041+lurenjia1213@users.noreply.github.com>
2024-10-31 23:05:34 +0800

    ...nothing done yet

--- os/src/sync/condvar.rs
+++ os/src/sync/condvar.rs
@@ -3,7 +3,7 @@
 use crate::sync::{Mutex, UPSafeCell};
 use crate::task::{block_current_and_run_next, current_task, wakeup_task, TaskControlBlock};
 use alloc::{collections::VecDeque, sync::Arc};
-
+//https://course.rs/advance/concurrency-with-threads/sync1.html#%E7%94%A8%E6%9D%A1%E4%BB%B6%E5%8F%98%E9%87%8Fcondvar%E6%8E%A7%E5%88%B6%E7%BA%BF%E7%A8%8B%E7%9A%84%E5%90%8C%E6%AD%A5
 /// Condition variable structure
 pub struct Condvar {
     /// Condition variable inner
--- os/src/sync/semaphore.rs
+++ os/src/sync/semaphore.rs
@@ -28,13 +28,16 @@ impl Semaphore {
             },
         }
     }
-
+/*
+互斥锁用 0 表示临界区已经被占用了，用 1 表示临界区为空，再通过 lock/unlock 操作来协调多个线程轮流独占临界区执行。
+而信号量的初始值可设置为 N 的整数变量, 如果 N 大于 0， 表示最多可以有 N 个线程进入临界区执行，如果 N 小于等于 0 ，表示不能有线程进入临界区了， 必须在后续操作中让信号量的值加 1 ，才能唤醒某个等待的线程。
+*/
     /// up operation of semaphore
     pub fn up(&self) {
         trace!("kernel: Semaphore::up");
         let mut inner = self.inner.exclusive_access();
-        inner.count += 1;
-        if inner.count <= 0 {
+        inner.count += 1;//释放资源
+        if inner.count <= 0 {//但 S 的取值范围可以是小于 0 的整数，表示等待进入临界区的睡眠线程数。
             if let Some(task) = inner.wait_queue.pop_front() {
                 wakeup_task(task);
             }
